b97c8ae469c93322d7317fe231bf5ef0d7d656ad
yacineb17120 <yacinefadlallah.bestaoui@univ-tlemcen.dz>
2021-03-21 17:47:13 +0100

    Adding Repositories,Bootstrap Init Database

---
+++ src/main/java/spring5learning/springframwork/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package spring5learning.springframwork.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import spring5learning.springframwork.spring5webapp.model.Author;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
